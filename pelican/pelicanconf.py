4e5ecb2d758f7d9b14dcf5233d7f7bcd1810defb
Andrey Semakin <and-semakin@ya.ru>
2020-04-19 16:03:42 +0500

    Move blog to custom domain semakin.dev

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -19,7 +19,7 @@ EXTRA_PATH_METADATA = {
     'extra/favicon_16.png': {'path': 'favicon_16.png'},
     'extra/favicon_24.png': {'path': 'favicon_24.png'},
     'extra/favicon_32.png': {'path': 'favicon_32.png'},
-    # 'extra/CNAME': {'path': 'CNAME'},
+    'extra/CNAME': {'path': 'CNAME'},
     # 'extra/LICENSE': {'path': 'LICENSE'},
     # 'extra/README': {'path': 'README'},
 }
