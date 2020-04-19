1efb177a66adb5f1da34f76d586764980164b74d
Andrey Semakin <and-semakin@ya.ru>
2020-04-19 13:57:10 +0500

    Add about page

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -9,7 +9,12 @@ ARTICLE_PATHS = ['blog']
 ARTICLE_SAVE_AS = '{date:%Y}/{slug}/index.html'
 ARTICLE_URL = '{date:%Y}/{slug}/'
 
+PAGE_PATHS = ['pages']
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+PAGE_URL = 'pages/{slug}/'
 DISPLAY_PAGES_ON_MENU = True
+
+STATIC_PATHS = ['static', 'pages']
 
 DEFAULT_METADATA = {
     'status': 'draft',
@@ -33,7 +38,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 # Social widget
 SOCIAL = (('github', 'https://github.com/and-semakin'),
           ('telegram', 'https://t.me/bro0ke'),
-          ('twitter', 'https://twitter.com/and_semakin'),)
+          ('linkedin', 'https://www.linkedin.com/in/%D0%B0%D0%BD%D0%B4%D1%80%D0%B5%D0%B9-%D1%81%D0%B5%D0%BC%D0%B0%D0%BA%D0%B8%D0%BD-501619108/'),)
 
 DEFAULT_PAGINATION = 10
 
