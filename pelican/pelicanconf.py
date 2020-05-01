77f801805c14417da5ad87f56c95cc0c966e8a9f
Andrey Semakin <and-semakin@ya.ru>
2020-05-01 12:03:26 +0500

    Change social links

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -49,9 +49,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Social widget
-SOCIAL = (('github', 'https://github.com/and-semakin'),
-          ('telegram', 'https://t.me/bro0ke'),
-          ('linkedin', 'https://www.linkedin.com/in/%D0%B0%D0%BD%D0%B4%D1%80%D0%B5%D0%B9-%D1%81%D0%B5%D0%BC%D0%B0%D0%BA%D0%B8%D0%BD-501619108/'),)
+SOCIAL = (('rss', '/feeds/all.atom.xml'),
+          ('telegram', 'https://t.me/pythonic_attacks'))
 
 DEFAULT_PAGINATION = 10
 
