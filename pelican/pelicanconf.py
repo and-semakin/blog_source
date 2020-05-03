17ccade7a5855eaa29ed57a8aa84cf2bd65d73df
Andrey Semakin <and-semakin@ya.ru>
2020-05-03 15:46:23 +0500

    Format everything with black

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -3,43 +3,43 @@ import functools
 import plural_ru
 
 
-AUTHOR = 'Андрей Семакин'
-SITENAME = 'Питонические атаки'
-SITESUBTITLE = 'Про разработку в целом и про Python в частности'
+AUTHOR = "Андрей Семакин"
+SITENAME = "Питонические атаки"
+SITESUBTITLE = "Про разработку в целом и про Python в частности"
 SHOW_SOCIAL_ON_INDEX_PAGE_HEADER = True
-SITEURL = ''
-SLUGIFY_SOURCE = 'basename'
+SITEURL = ""
+SLUGIFY_SOURCE = "basename"
 
-PATH = 'content'
-ARTICLE_PATHS = ['blog']
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}/index.html'
-ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
+PATH = "content"
+ARTICLE_PATHS = ["blog"]
+ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{slug}/index.html"
+ARTICLE_URL = "{date:%Y}/{date:%m}/{slug}/"
 
-PAGE_PATHS = ['pages']
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
-PAGE_URL = 'pages/{slug}/'
+PAGE_PATHS = ["pages"]
+PAGE_SAVE_AS = "pages/{slug}/index.html"
+PAGE_URL = "pages/{slug}/"
 DISPLAY_PAGES_ON_MENU = True
 
-STATIC_PATHS = ['static', 'extra', 'pages']
+STATIC_PATHS = ["static", "extra", "pages"]
 EXTRA_PATH_METADATA = {
-    'extra/favicon_16.png': {'path': 'favicon_16.png'},
-    'extra/favicon_24.png': {'path': 'favicon_24.png'},
-    'extra/favicon_32.png': {'path': 'favicon_32.png'},
-    'extra/CNAME': {'path': 'CNAME'},
-    'extra/.nojekyll': {'path': '.nojekyll'},
-    'extra/README.md': {'path': 'README.md'},
-    'extra/robots.txt': {'path': 'robots.txt'},
+    "extra/favicon_16.png": {"path": "favicon_16.png"},
+    "extra/favicon_24.png": {"path": "favicon_24.png"},
+    "extra/favicon_32.png": {"path": "favicon_32.png"},
+    "extra/CNAME": {"path": "CNAME"},
+    "extra/.nojekyll": {"path": ".nojekyll"},
+    "extra/README.md": {"path": "README.md"},
+    "extra/robots.txt": {"path": "robots.txt"},
 }
 
-DEFAULT_CATEGORY = 'blog'
+DEFAULT_CATEGORY = "blog"
 DISPLAY_CATEGORIES_ON_MENU = False
 DEFAULT_METADATA = {
-    'status': 'draft',
+    "status": "draft",
 }
 
-TIMEZONE = 'Asia/Yekaterinburg'
+TIMEZONE = "Asia/Yekaterinburg"
 
-DEFAULT_LANG = 'ru'
+DEFAULT_LANG = "ru"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -49,47 +49,40 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Social widget
-SOCIAL = (('rss', '/feeds/all.atom.xml'),
-          ('telegram', 'https://t.me/pythonic_attacks'))
+SOCIAL = (("rss", "/feeds/all.atom.xml"), ("telegram", "https://t.me/pythonic_attacks"))
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-THEME = 'themes/pelican-clean-blog'
-COLOR_SCHEME_CSS = 'monokai.css'
+THEME = "themes/pelican-clean-blog"
+COLOR_SCHEME_CSS = "monokai.css"
 
-UTTERANCES_REPO = 'and-semakin/and-semakin.github.io'
+UTTERANCES_REPO = "and-semakin/and-semakin.github.io"
 
 PLUGIN_PATHS = ["plugins"]
 PLUGINS = ["sitemap", "deadlinks", "filetime_from_git", "post_stats"]
 
 SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.5,
-        'indexes': 0.5,
-        'pages': 0.5
-    },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly'
-    }
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+    "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
 }
 DEADLINK_VALIDATION = False
 DEADLINK_OPTS = {
-    'archive': True,
-    'classes': [],
-    'labels': False,
-    'timeout_duration_ms': 10000,
-    'timeout_is_error': True,
+    "archive": True,
+    "classes": [],
+    "labels": False,
+    "timeout_duration_ms": 10000,
+    "timeout_is_error": True,
 }
 
 GIT_FILETIME_FROM_GIT = True
 
 SHOW_READ_TIME = True
-PLURAL_MINUTE = functools.partial(plural_ru.ru, quantitative=["минута", "минуты", "минут"])
+PLURAL_MINUTE = functools.partial(
+    plural_ru.ru, quantitative=["минута", "минуты", "минут"]
+)
 
 DISABLE_CUSTOM_THEME_JAVASCRIPT = False
--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -3,22 +3,23 @@
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://semakin.dev'
+SITEURL = "https://semakin.dev"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
 YANDEX_METRIKA_ID = "62079343"
 
 DEADLINK_VALIDATION = True
